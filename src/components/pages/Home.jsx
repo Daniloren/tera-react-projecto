6cbd00084521b4436f18e486005e0e8fdad2a398
Daniella Furquim <daniella.lorenzano@gmail.com>
2022-10-17 17:55:49 -0300

    migra HTML dos componentes da pagina inicial

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import logo from "../../images/logo.svg";
 
 export default function Home() {
   return (
     <div className="home center">
     <div className="home__logo">
-      <img src="images/logo.svg" class="responsive" alt="" />
+      <img src={logo}className="responsive" alt="" />
     </div>
     <select className="home__select-users">
       <option>User 1</option>
@@ -13,5 +14,7 @@ export default function Home() {
     </select>
     <button className="button-primary">Entrar</button>
   </div>
-  )
+  );
 }
+
+
