3b9d6036b0b91f2029542f6ff0ca4da1dbfe783b
Daniella Furquim <daniella.lorenzano@gmail.com>
2022-10-18 11:00:34 -0300

    tranferencia de hml para react

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Home from "./components/pages/Home";
+import UserBlog from "./components/pages/UserBlog";
 
 import "./styles/normalize.css";
 import "./styles/fontawesome.min.css";
 import "./styles/main.css";
 
 function App() {
-  return <Home />;
+  return <UserBlog />;
 }
 
 export default App;
